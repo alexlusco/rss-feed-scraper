560ed8321c5e7d3ef0f421d0abfe4e426b0904b1
Alex Luscombe <44949344+alexlusco@users.noreply.github.com>
2020-12-18 13:56:15 -0500

    Update scrape_local.R

--- scrape_local.R
+++ scrape_local.R
@@ -10,36 +10,36 @@ for (r in rss_urls){
   
   ids_sheet <- suppressMessages(read_sheet(sheet_url, sheet = "IDs") %>% mutate(Title = as.character(Title)))
 
-  ids <- xml2::read_html(r) %>% #these tags refer to google alert RSS feeds, may need to alter for other feeds
-    rvest::html_nodes("entry") %>%
-    rvest::html_nodes("id") %>%
-    rvest::html_text(trim = TRUE)
+  ids <- read_html(r) %>% #these tags refer to google alert RSS feeds, may need to alter for other feeds
+    html_nodes("entry") %>%
+    html_nodes("id") %>%
+    html_text(trim = TRUE)
   
-  titles <- xml2::read_html(r) %>%
-    rvest::html_nodes("entry") %>%
-    rvest::html_nodes("title") %>%
-    rvest::html_text(trim = TRUE)
+  titles <- read_html(r) %>%
+    html_nodes("entry") %>%
+    html_nodes("title") %>%
+    html_text(trim = TRUE)
     
-  links <- xml2::read_html(r) %>%
-    rvest::html_nodes("entry") %>%
-    rvest::html_nodes("link") %>%
-    rvest::html_attr("href")
+  links <- read_html(r) %>%
+    html_nodes("entry") %>%
+    html_nodes("link") %>%
+    html_attr("href")
   
-  abstracts <- xml2::read_html(r) %>%
-    rvest::html_nodes("entry") %>%
-    rvest::html_nodes("content") %>%
-    rvest::html_text(trim = TRUE) %>%
+  abstracts <- read_html(r) %>%
+    html_nodes("entry") %>%
+    html_nodes("content") %>%
+    html_text(trim = TRUE) %>%
     as.character()
   
-  published <- xml2::read_html(r) %>%
-    rvest::html_nodes("entry") %>%
-    rvest::html_nodes("published") %>%
-    rvest::html_text(trim = TRUE)
+  published <- read_html(r) %>%
+    html_nodes("entry") %>%
+    html_nodes("published") %>%
+    html_text(trim = TRUE)
   
-  updated <- xml2::read_html(r) %>%
-    rvest::html_nodes("entry") %>%
-    rvest::html_nodes("updated") %>%
-    rvest::html_text(trim = TRUE)
+  updated <- read_html(r) %>%
+    html_nodes("entry") %>%
+    html_nodes("updated") %>%
+    html_text(trim = TRUE)
   
   df_alerts <- tibble("Added" = now(),
                       "Reviewed" = NA,
